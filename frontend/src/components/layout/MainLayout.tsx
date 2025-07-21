1fb2f6ce523a8773137af740ebe46e11d5c05df8
lordbaah <lordbaah2@gmail.com>
2025-07-21 22:46:32 +0000

    worked on blog fetching

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -7,7 +7,8 @@ interface MainLayoutProps {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div className="font-roboto antialiased text-gray-800 bg-gray-50">
+    // min-h-screen helps to let footer always be at the button
+    <div className="flex flex-col min-h-screen font-roboto antialiased text-gray-800 bg-gray-50">
       <Navbar />
       <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
       <Footer />
